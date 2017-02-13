0092bcd6d6ed7ae608e02b4f7d8d80b606feb752
Is Ma <dotpointing@gmail.com>
2017-02-12 21:45:12 -0600

    Fix code: allow an unexpected 'nil' value as argument

--- lib/hello_planet.rb
+++ lib/hello_planet.rb
@@ -1,7 +1,8 @@
 require_relative './hello_planet/version'
 
 module HelloPlanet
-  def self.hi(to_what)
+  def self.hi(to_what = nil)
+    to_what = to_what || 'planet'
     "Hello #{to_what}!"
   end
 end
